98688de69b33a5d67d2cee3166110253b9a46eb4
ClicShopping <Yae8mr2o*>
2021-01-28 16:00:21 +0100

    update template

--- includes/ClicShopping/Apps/Tools/AppComposer/Sites/ClicShoppingAdmin/Pages/Home/templates/configure.php
+++ includes/ClicShopping/Apps/Tools/AppComposer/Sites/ClicShoppingAdmin/Pages/Home/templates/configure.php
@@ -96,7 +96,7 @@
             echo HTML::button($CLICSHOPPING_AppComposer->getDef('button_save'), null, null, 'success');
 
             if ($CLICSHOPPING_AppComposer->getConfigModuleInfo($current_module, 'is_uninstallable') === true) {
-              echo '<span class="float-md-right">' . HTML::button($CLICSHOPPING_AppComposer->getDef('button_dialog_uninstall'), null, '#', 'warning', ['params' => 'data-toggle="modal" data-target="#ppUninstallModal"']) . '</span>';
+              echo '<span class="float-end">' . HTML::button($CLICSHOPPING_AppComposer->getDef('button_dialog_uninstall'), null, '#', 'warning', ['params' => 'data-toggle="modal" data-target="#ppUninstallModal"']) . '</span>';
             }
           ?>
         </div>
